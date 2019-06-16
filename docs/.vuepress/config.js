9d25cfb1fa4ad77c346f2e57f1149ac35c2b1f22
ff-stone <dreamersfly@163.com>
2019-06-16 14:28:41 +0800

    🚁  update(sidebar): add sidebar config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -33,5 +33,117 @@ module.exports = {
         lineNumbers: true,
         // 目录渲染层级
         toc: { includeLevel: [2, 3] }
+    },
+    themeConfig: {
+        // 顶部导航，如果需要禁用导航，需要设置 navbar: false
+        nav: [
+            /**
+             * 如果需要使用多级导航，示例：
+             * {
+                    text: "Languages",
+                    items: [
+                        { text: "Chinese", link: "/language/chinese" },
+                        { text: "Japanese", items: [
+                            { text: "Home", link: "/" }
+                        ] }
+                    ]
+                }
+             */
+            { text: "Home", link: "/" },
+            /**
+             * 如果 link 是一个目录，将会默认指向该目录下的 README.md
+             */
+            { text: "Introduction", link: "/Introduction/" }
+        ],
+        /**
+         *  侧边栏
+         * 如果希望自动生成仅包含当前页面的标题链接的侧边栏，可以设置 sidebar: auto.
+         * 如果希望在某个页面上设置，还可以如下所示：
+         * '/': {
+            sidebar: 'auto'
+            }
+         * 设置为 false 表示禁止
+         */
+        sidebar: [
+            "/",
+            /**
+             * 你可以省略 .md 扩展名，以 / 结尾的路径被推断为 *\/README.md 。
+             * 该链接的文本是自动推断的（从页面的第一个标题或 YAML front matter 中的显式标题）。
+             * 如果你希望明确指定链接文本，请使用 \[link,text] 形式的数组。
+             */
+            "/Introduction/",
+            /**
+             * 侧边栏可以分组，形式如下
+             */
+            {
+                title: "官方文档",
+                collapsable: false,
+                children: [
+                    ["https://www.vuepress.cn/", "VuePress"],
+                    [
+                        "https://www.vuepress.cn/default-theme-config/",
+                        "默认主题配置"
+                    ]
+                ]
+            }
+        ],
+
+        /**
+         * 我们还可以为页面单独定制侧边栏，示例如下
+            sidebar: {
+                '/foo/': [
+                    '',      /foo/ 
+                    'one',   /foo/one.html 
+                    'two'    /foo/two.html 
+                ],
+
+                '/bar/': [
+                    '',       /bar/ 
+                    'three',  /bar/three.html 
+                    'four'    /bar/four.html 
+                ],
+
+                // 回退(fallback)侧边栏配置
+                '/': [
+                    '',         / 
+                    'contact',  /contact.html 
+                    'about'     /about.html 
+                ]
+            }
+         */
+
+        // 侧边栏自动显示当前激活页面中标题的链接，嵌套在页面本身的链接下
+        sidebarDepth: 1,
+        /**
+         * 默认情况下，侧边栏只会显示由当前活动页面的标题（headers）组成的链接，
+         * 你可以将 themeConfig.displayAllHeaders 设置为 true 来显示所有页面的标题链接：
+         */
+        displayAllHeaders: false,
+        /**
+         * 默认情况下，当用户滚动页面，查看不同部分时，
+         * 嵌套的标题链接和 URL 中的哈希值会随之更新，
+         * 此行为可以通过以下的主题配置来禁用：
+         */
+        activeHeaderLinks: true,
+
+        /**
+         * 表示是否禁用搜索框
+         * 如果你需要全文搜索，内置搜索只能从标题 h2 和 h3 标题构建索引，
+         * 你可以使用 [Algolia](https://www.vuepress.cn/default-theme-config/#algolia-search) 搜索。
+         *  algolia: {
+                apiKey: '<API_KEY>',
+                indexName: '<INDEX_NAME>'
+            }
+         */
+
+        search: true,
+        searchMaxSuggestions: 10,
+
+        /**
+         *  lastUpdated: 选项允许你获取每个文件的最后一次 git 提交的 UNIX 时间戳（ms），
+         * 并且它也会以合适的格式显示在每个页面的底部
+         * 请注意，它默认是关闭的，如果给定一个 string 类型的值，它将会作为前缀显示（默认值是：Last Updated）。
+         */
+        lastUpdated: "更新时间："
     }
 };
---
+++ docs/Introduction/README.md
@@ -0,0 +1,46 @@
+# VuePress Template
+
+[VuePress](https://www.vuepress.cn/): Vue 驱动的静态站点生成工具
+
+[[toc]]
+
+## 目录结构介绍
+
+```shell
+.
+├── .gitignore
+├── README.md
+├── docs
+│   ├── .vuepress
+│   │   ├── config.js
+│   │   └── enhanceApp.js
+│   └── README.md
+├── package-lock.json
+└── package.json
+```
+
+-   `./docs/.vuepress` 用于放置所有 VuePress 特有文件的地方
+-   `./docs/.vuepress/config.js` 是站点基本文件，输入 JS 对象
+-   `./docs/.vuepress/enhanceApp.js` 应用程序级别的增强功能
+
+## 功能介绍
+
+### 搜索
+
+VuePress 带有内置的 基于 header 搜索(`headers-based search`) 的功能 - 它会自动从所有页面的 title, h2 和 h3 header 标签中，建立一个简单的搜索索引。（译者注：此搜索是遍历 pages 数组的每一项，取出其中的 headers 数组，其中数组每项对应 markdown 标题，具体代码查看 /lib/default-theme/SearchBox.vue 的 suggestions 函数）
+
+### 静态资源处理
+
+[静态资源](https://www.vuepress.cn/guide/assets.html#%E7%9B%B8%E5%AF%B9-urls)将有 webpack 处理，我们可以使用相对路径，将其交给 webpack。还可以使用 webpack 别名来引用文件。我们可以在 `.vuepress/config.js` 中的 `configureWebpack` 字段里进行配置。
+
+1. 公共文件
+
+    `.vuepress/public` 中的文件将被复制到生成目录的根目录。
+
+2. 基准 URL
+
+    如果你的网站部署到非根 URL，则需要在 .vuepress/config.js 中设置 base 选项。
+
+## 主题介绍
+
+VuePress 附带一个默认主题（你现在看到的就是），专门为技术文档设计。它暴露许多选项，允许你自定义导航栏(navbar)、侧边栏(sidebar)和主页(homepage)等。详细信息请查看 [默认主题配置](https://www.vuepress.cn/default-theme-config/#%E5%88%B7%E6%96%B0%E5%86%85%E5%AE%B9%E7%9A%84%E5%BC%B9%E7%AA%97) 页面。
