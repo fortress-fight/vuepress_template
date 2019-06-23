ab4caf3435f6401c404eb48f07587ba59feaf5ac
ff-stone <dreamersfly@163.com>
2019-06-23 18:24:58 +0800

    🚀  feature(components): add sitebg components

---
+++ docs/.vuepress/components/sitebg.vue
@@ -0,0 +1,74 @@
+<template>
+    <div id="site_bg" class="site_bg" :style="c_style">
+        <img class="site_bg-img" :src="bg_link" alt srcset>
+    </div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+    data() {
+        return {
+            bg_link: ""
+        };
+    },
+    props: {
+        bg: String
+    },
+    computed: {
+        c_style() {
+            return {
+                backgroundImage: `url(${this.bg_link})`
+            };
+        }
+    },
+    mounted() {
+        this.bg_link = this.$withBase(
+            this.bg || this.$page.frontmatter.heroImage
+        );
+    }
+});
+</script>
+
+<style lang="scss">
+.home .hero {
+    height: calc(100vh - 11.6rem);
+
+    position: relative;
+    z-index: 10;
+    text-align: right;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin-left: calc(50% - 50vw);
+    img {
+        display: none;
+    }
+    .description {
+        margin: 0;
+        width: 100%;
+        max-width: none;
+    }
+}
+.home .footer {
+    position: relative;
+    z-index: 10;
+}
+#main-title {
+    max-width: 100%;
+    width: 100%;
+}
+.site_bg {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-position: center;
+    background-size: cover;
+    &-img {
+        display: none;
+    }
+}
+</style>
+
--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,3 +1,7 @@
+import jquery from "jquery";
+import ClipboardJS from "clipboard";
+import "./styles/font-awesome.css";
+
 export default ({
     Vue, // 当前 VuePress 应用所使用的 Vue 版本
     options, // 根 Vue 实例的选项
@@ -5,5 +9,26 @@ export default ({
     siteData // 网站元数据
 }) => {
     // ...使用应用程序级别的增强功能
-    console.log("应用增强");
+    window.$ = jquery;
+    console.log(this);
+    router.afterHooks.push(function() {
+        Vue.nextTick(dom_loaded);
+    });
 };
+
+function dom_loaded() {
+    // setTimeout(() => {}, 200);
+    $("body").on("mouseenter", "pre", function() {
+        let copy_btn = $(this).find(".copy_btn");
+        if (!copy_btn.length) {
+            copy_btn = $(
+                "<div class='copy_btn'><i class='fa fa-copy'></div>"
+            ).appendTo(this);
+            new ClipboardJS(copy_btn[0], {
+                target: trigger => {
+                    return $(this).find("code")[0];
+                }
+            });
+        }
+    });
+}
